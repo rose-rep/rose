eaaae56bfaec877b2eac8c24917d95cf351ab6ca
Dmitry Akentyev <TogusaRusso@gmail.com>
2023-04-24 14:50:08 +0300

    Fact - menu update

---
+++ .bottom_menu_R_RU.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Партнерская программа", 
+		"/loyalty/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Сертификаты и гарантии", 
+		"/sertifikaty-i-garantii/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Технические характеристики продукции", 
+		"/specifications/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
